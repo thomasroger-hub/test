7529ffd4eea3bee326be50e88e966c5e707735bb
thomasroger-hub <thomasroger88888@gmail.com>
2025-07-17 01:00:59 +0800

    fix typo

--- hello.java
+++ hello.java
@@ -3,4 +3,4 @@ master user add a new line2 to hello.java
 master user add a new line3 to hello.java
 feature-A user add a new line4 to hello.java 
 fix-B user add a new line5 to hello.java
-faeture-C user add a new line6 to hello.java
+feature-C user add a new line6 to hello.java
